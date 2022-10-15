390b0f1bbf313b8fdb323b2a2e31da78f04c1670
Lucas Delvoye <lucas02delvoye@gmail.com>
2022-10-15 14:33:48 -0400

    testing bot build

--- commands/vote.ts
+++ commands/vote.ts
@@ -1,24 +1,16 @@
-const { SlashCommandBuilder } = require('discord.js')
+import { SlashCommandBuilder } from 'discord.js'
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('vote')
         .setDescription('Adds a vote to the user of your choice.')
-        .addUserOption( option: any =>
+        .addUserOption( option =>
             option
                 .setName('user')
                 .setDescription('The user you want to vote for')
-<<<<<<< HEAD
-                .setRequired(true)
-            ),
-    async execute(interaction: any) {
-        
-        return interaction.reply('')
-=======
                 .setRequired(true)),
     async execute(interaction: any) {
 
         return interaction.reply('Hello')
->>>>>>> 9529f5a7c5a840aa64f5e968dad79b67f127b204
     }
 }
