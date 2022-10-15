6d36f2dcf6fd6431434d170934077d581f3b899b
Syed Muhammad Zafar Alam <alam.syedmz@gmail.com>
2022-10-15 14:17:08 -0400

    added commands folder

---
+++ commands/vote.ts
@@ -0,0 +1,16 @@
+import { SlashCommandBuilder } from 'discord.js'
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('vote')
+        .setDescription('Adds a vote to the user of your choice.')
+        .addUserOption( option =>
+            option
+                .setName('user')
+                .setDescription('The user you want to vote for')
+                .setRequired(true))
+    async execute(interaction: any) {
+
+        await interaction.reply('')
+    }
+}
