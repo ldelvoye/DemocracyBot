0e8d6409114b54caa8e52fac4fa779a7fcae00c1
Syed Muhammad Zafar Alam <alam.syedmz@gmail.com>
2022-10-15 14:53:21 -0400

    ok

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const client = new Client({
 })
 
 client.commands = new Collection()
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endswith('js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file):;
+        const command = require(filePath)
+        client.commands.set(command.data.name, command)
+}
 
 client.on('messageCreate', (message) => {
     if (message.content === 'ping') {
