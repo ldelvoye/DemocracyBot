2767ab933cb4492d4cf4faa7db825f7284a70524
Lucas Delvoye <lucas02delvoye@gmail.com>
2022-10-15 15:17:21 -0400

    setting up basic slash command response

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client, Collection, GatewayIntentBits } = require('discord.js')
 const fs = require('node:fs')
 const path = require('node:path')
-require('./config.json').config();
+const {token} = require('./config.json')
 
 const client = new Client({
     intents: [
@@ -11,21 +11,29 @@ const client = new Client({
     ]
 })
 
-client.commands = new Collection()
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endswith('js'));
+// client.commands = new Collection()
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('js'));
 
-for (const file of commandFiles) {
-    const filePath = path.join(commandsPath, file);
-        const command = require(filePath)
-        client.commands.set(command.data.name, command)
-}
+// for (const file of commandFiles) {
+//     const filePath = path.join(commandsPath, file);
+//         const command = require(filePath)
+//         client.commands.set(command.data.name, command)
+// }
 
-client.on('messageCreate', (message) => {
-    if (message.content === 'ping') {
-        message.channel.send('pong')
-    }
-})
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'server') {
+		await interaction.reply('Server info.');
+	} else if (commandName === 'user') {
+		await interaction.reply('User info.');
+	}
+});
 
 client.on('ready', () => {
     console.log(`Logged in as user ${client.user?.username}`)
