0f8faa6079098b9de1762c53fd0f0ec1e3b1d954
Lucas Delvoye <lucas02delvoye@gmail.com>
2022-10-15 17:51:59 -0400

    Vote command embed format

--- commands/vote.js
+++ commands/vote.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, userMention, CommandInteractionOptionResolver } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,11 +10,16 @@ module.exports = {
                 .setRequired(true)
             ),
     async execute(interaction) {
-
         const voter = interaction.user
         const voterId = voter.id
         const votee = interaction.options.getUser('user')
         const voteeId = votee.id
-        await interaction.reply(`${voter} voted for ${votee}`)
+
+        const message = new EmbedBuilder()
+            .setTitle('You just cast your vote')
+            .setDescription(`${voter} voted for ${votee}`)
+            .setColor(0x00FF00)
+
+        await interaction.reply({embeds: [message]})
     }
 }
