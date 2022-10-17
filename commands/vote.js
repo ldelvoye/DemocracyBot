7fae39dff618149152cc9f0c3c566b965c7eb3cb
Lucas Delvoye <delvoye02lucas@gmail.com>
2022-10-17 18:20:19 -0400

    fixing vote

--- commands/vote.js
+++ commands/vote.js
@@ -1,43 +1,34 @@
-// const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-// const {
-//   updateVotes,
-//   updateLeaderboard,
-// } = require("../database/bot_db_operations");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const {
+  updateVotes,
+  updateLeaderboard,
+} = require("../database/bot_db_operations");
 
-// module.exports = {
-//   data: new SlashCommandBuilder()
-//     .setName("vote")
-//     .setDescription("Cast or change your vote!")
-//     .addUserOption((option) =>
-//       option
-//         .setName("user")
-//         .setDescription("Who you are voting for")
-//         .setRequired(true)
-//     ),
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("vote")
+    .setDescription("Cast or change your vote!")
+    .addUserOption((option) =>
+      option
+        .setName("user")
+        .setDescription("Who you are voting for")
+        .setRequired(true)
+    ),
 
-//   async execute(interaction) {
-//     const voter = interaction.user;
-//     const voterId = voter.id;
-//     const votee = interaction.options.getUser("user");
-//     const voteeId = votee.id;
+  async execute(interaction) {
+    const voter = interaction.user;
+    const voterId = voter.id;
+    const votee = interaction.options.getUser("user");
+    const voteeId = votee.id;
 
-//     /* 
-//       first check if voter exists in db
-//         if does, then update voteeID
-//         go to voteeID and update their count
-//       if does not exist then
-//         create new entry in db with voterid, voteeid, vote, and leader=false
-//         go to voteeID and update count
-//     */
+    updateVotes(voterId, voteeId);
+    updateLeaderboard();
 
-//     updateVotes(voterId, voteeId);
-//     updateLeaderboard();
+    const message = new EmbedBuilder()
+      .setTitle("You just cast your vote")
+      .setDescription(`${voter} voted for ${votee}`)
+      .setColor(0x00ff00);
 
-//     const message = new EmbedBuilder()
-//       .setTitle("You just cast your vote")
-//       .setDescription(`${voter} voted for ${votee}`)
-//       .setColor(0x00ff00);
-
-//     await interaction.reply({ embeds: [message] });
-//   },
-// };
+    await interaction.reply({ embeds: [message] });
+  },
+};
