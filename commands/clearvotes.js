a0b4722c01fd582cff22b9858e2e3f2d1862e34b
Lucas Delvoye <delvoye02lucas@gmail.com>
2022-10-18 16:20:57 -0400

    formatting

--- commands/clearvotes.js
+++ commands/clearvotes.js
@@ -1,17 +1,18 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
-const resetLeaderboard = require("../database/bot_db_operations")
+// const resetLeaderboard = require("../database/bot_db_operations")
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('clearvotes'),
-    
-    async execute(interaction) {
-        console.log('resetting leaderboard')
+  data: new SlashCommandBuilder()
+    .setName('clearvotes')
+    .setDescription('Clears the leaderboard -admin command'),
+  
+  async execute(interaction) {
+    console.log('resetting leaderboard')
 
-        const message = new EmbedBuilder()
-            .setTitle('Leaderboard successfully reset')
-            .setColor(0xA020F0)
-        
-        await interaction.reply({ embeds: [message] })
-    }
+    const message = new EmbedBuilder()
+      .setTitle('Leaderboard successfully reset')
+      .setColor(0xA020F0)
+    
+    await interaction.reply({ embeds: [message] })
+  }
 }
--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,22 @@
 const { SlashCommandBuilder, EmbedBuilder} = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('help')
-        .setDescription('Command lists & what they do'),
-    
-    async execute(interaction) {
-        const message = new EmbedBuilder()
-            .setTitle('Help - Commands')
-            .setColor(0x89CFF0)
-            .addFields(
-                {name: `/vote`, value: `Vote for the server's leader`},
-                {name: `/leader`, value: `Various information about the server`},
-                {name: `/help`, value: `Show the help menu`},
-                {name: `\u200B`, value: '-- Leader-only commands --'},
-                {name: `/banvote`, value: `Begin a ban vote, the peasants will get 24 hours to vote`}
-            )
-        
-            await interaction.reply({embeds:[message]})
-    }
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Command lists & what they do'),
+  
+  async execute(interaction) {
+    const message = new EmbedBuilder()
+      .setTitle('Help - Commands')
+      .setColor(0x89CFF0)
+      .addFields(
+        {name: `/vote`, value: `Vote for the server's leader`},
+        {name: `/leader`, value: `Various information about the server`},
+        {name: `/help`, value: `Show the help menu`},
+        {name: `\u200B`, value: '-- Leader-only commands --'},
+        {name: `/banvote`, value: `Begin a ban vote, the peasants will get 24 hours to vote`}
+      )
+  
+      await interaction.reply({embeds:[message]})
+  }
 }
