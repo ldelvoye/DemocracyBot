db6d4850118268186e3f9fa51fc98b4d599f729a
Syed Muhammad Zafar Alam <alam.syedmz@gmail.com>
2022-10-18 19:56:53 -0400

    added goodbye commands

--- modules/goodbye.js
+++ modules/goodbye.js
@@ -1,6 +1,9 @@
-module.exports = client => {
-    client.on('guildMemberRemove', (member) => {
-        const channel = member.guild.channels.cache.get('1031325406005055513')
-        channel.send(`Goodbye ${member}, their vote has been removed.`)
-    })
-}
+const { removeVoter } = require("../database/bot_db_operations");
+
+module.exports = (client) => {
+  client.on("guildMemberRemove", (member) => {
+    const channel = member.guild.channels.cache.get("1031325406005055513");
+    removeVoter(member.id);
+    channel.send(`Goodbye ${member}, their vote has been removed.`);
+  });
+};
