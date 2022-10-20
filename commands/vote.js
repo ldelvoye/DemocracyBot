f38a054019cde352a82950011f0bee4e8f1363e7
Syed Muhammad Zafar Alam <alam.syedmz@gmail.com>
2022-10-20 15:42:54 -0400

    modified vote

--- commands/vote.js
+++ commands/vote.js
@@ -1,7 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const {
-  updateVotes,
-  updateLeaderboard,
+  // updateVotes,
+  // updateLeaderboard,
+  voteCommandDB,
   getCurrentLeader,
 } = require("../database/bot_db_operations");
 
@@ -22,45 +23,51 @@ module.exports = {
     const votee = interaction.options.getUser("user");
     const voteeId = votee.id;
 
-    const message = new EmbedBuilder()
-      if (votee.bot === true) {
-        message.setTitle("Error")
-        message.setDescription(`You cannot vote for a bot`)
-        message.setColor(0x7CFC00)
-      } else {
-        message.setTitle("You just cast your vote")
-        message.setDescription(`${voter} voted for ${votee}`)
-        message.setColor(0x00ff00)
-        updateVotes(voterId, voteeId);
-      }
+    const message = new EmbedBuilder();
+    if (votee.bot === true) {
+      message.setTitle("Error");
+      message.setDescription(`You cannot vote for a bot`);
+      message.setColor(0x7cfc00);
+    } else {
+      message.setTitle("You just cast your vote");
+      message.setDescription(`${voter} voted for ${votee}`);
+      message.setColor(0x00ff00);
+      // updateVotes(voterId, voteeId);
+    }
 
-    await interaction.reply({ embeds: [message] })
+    await interaction.reply({ embeds: [message] });
 
-    let temp = await updateLeaderboard()
-    let leaders = Object.values(temp)
-    let oldLeaderId = leaders[0]
-    let newLeaderId = leaders[1]
+    let temp = await voteCommandDB(voterId, voteeId);
+    let leaders = Object.values(temp);
+    let oldLeaderId = leaders[0];
+    let newLeaderId = leaders[1];
 
     if (newLeaderId > 0) {
-      let rolePleb = interaction.guild.roles.cache.find(r => r.id === "1031334547591274556")
-      let roleLeader = interaction.guild.roles.cache.find(r => r.id === "1031329937287807046")
+      let rolePleb = interaction.guild.roles.cache.find(
+        (r) => r.id === "1031334547591274556"
+      );
+      let roleLeader = interaction.guild.roles.cache.find(
+        (r) => r.id === "1031329937287807046"
+      );
 
       if (oldLeaderId > 0) {
-        let oldLeader = await interaction.guild.members.fetch(oldLeaderId)
-        oldLeader.roles.add(rolePleb)
-        oldLeader.roles.remove(roleLeader)
+        let oldLeader = await interaction.guild.members.fetch(oldLeaderId);
+        oldLeader.roles.add(rolePleb);
+        oldLeader.roles.remove(roleLeader);
       }
 
-      let newLeader = await interaction.guild.members.fetch(newLeaderId)
-      newLeader.roles.add(roleLeader)
-      newLeader.roles.remove(rolePleb)
+      let newLeader = await interaction.guild.members.fetch(newLeaderId);
+      newLeader.roles.add(roleLeader);
+      newLeader.roles.remove(rolePleb);
 
       const leaderChangeMessage = new EmbedBuilder()
         .setTitle("New Leader!")
-        .setDescription(`Congratulations, ${newLeader}, you are the new leader!`)
-        .setColor(0x7DF9FF)
+        .setDescription(
+          `Congratulations, ${newLeader}, you are the new leader!`
+        )
+        .setColor(0x7df9ff);
 
-      await interaction.followUp({ embeds: [leaderChangeMessage]})
+      await interaction.followUp({ embeds: [leaderChangeMessage] });
     }
   },
 };
